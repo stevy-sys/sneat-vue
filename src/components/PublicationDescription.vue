b01e882653a03fa4158c0cc17c7868a44d303de6
stevy <stevyralambomanana@gmail.com>
2023-09-24 16:26:53 +0200

    text description pub

---
+++ src/components/PublicationDescription.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { onMounted } from 'vue';
+
+const props = defineProps({
+    actionType: {
+        type: String,
+    }
+})
+
+const text = ref('')
+
+const getType = () => {
+    console.log(props.actionType)
+    switch (props.actionType) {
+        case 'statut':
+            text.value = 'John Doe a actualise un statut'
+            break;
+        case 'statutMedia':
+            text.value = 'John Doe a actualise un photo'
+            break;
+        case 'shareStatut':
+            text.value = 'John Doe a partage un statut de Jane Doe'
+            break;
+        case 'shareStatuMedia':
+            text.value = 'John Doe a partage un photo de Jane Doe'
+            break;
+        case 'sharePubGroupe':
+            text.value = 'John Doe > Gasikaratsika'
+            break;
+
+        default:
+            break;
+    }
+}
+onMounted(() => {
+    getType()
+})
+
+</script>
+
+<template>
+    <span class="ms-2 avatar-name">{{ text }}</span>
+</template>
+
+<style lang="scss">
+</style>
